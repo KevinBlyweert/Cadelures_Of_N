40f60d77b783cbe94d0fdc97c1aa092154501454
Neithan et Eldalote Coemgen <kevin.blyweert@gmail.com>
2024-05-31 15:54:20 +0200

    removed hover on cards, added real pictures

--- js/script.js
+++ js/script.js
@@ -1,11 +1,23 @@
 'use strict';
+
 const app = {
+    createImages: () => {
+        const grid = document.querySelector(".grid");
+        for (let index = 1; index <= 6; index++) {
+            const link = `/assets/images/calligraphies/calli${index}.jpg`;
+            const cardTemplate = document.querySelector("#card-template");
+            const cloneCard = cardTemplate.content.cloneNode(true);
+            cloneCard.querySelector("img").src = link;
+            grid.append(cloneCard);
+        }
+    },
     toggleTheme: (event) => {
         document.body.classList.toggle("light");
         document.body.classList.toggle("dark");
     },
     init: () => {
-        document.querySelector(".themeToggle").addEventListener("click", app.toggleTheme)
+        document.querySelector(".themeToggle").addEventListener("click", app.toggleTheme);
+        app.createImages();
     }
 }
 
